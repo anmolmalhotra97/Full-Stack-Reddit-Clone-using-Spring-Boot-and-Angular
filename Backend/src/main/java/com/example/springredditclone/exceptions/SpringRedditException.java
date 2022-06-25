2b3037aad4c7fb5710ee66103c653f099884ca58
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-06-24 22:17:26 -0400

    Created Vote service

--- Backend/src/main/java/com/example/springredditclone/exceptions/SpringRedditException.java
+++ Backend/src/main/java/com/example/springredditclone/exceptions/SpringRedditException.java
@@ -1,6 +1,10 @@
 package com.example.springredditclone.exceptions;
 
 public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exceptionMessage, Exception exception) {
+        super(exceptionMessage, exception);
+    }
+
     public SpringRedditException(String exceptionMessage) {
         super(exceptionMessage);
     }
--- Backend/src/main/java/com/example/springredditclone/mapper/PostMapper.java
+++ Backend/src/main/java/com/example/springredditclone/mapper/PostMapper.java
@@ -5,23 +5,52 @@ import com.example.springredditclone.dto.PostResponse;
 import com.example.springredditclone.model.Post;
 import com.example.springredditclone.model.Subreddit;
 import com.example.springredditclone.model.User;
+import com.example.springredditclone.repository.CommentRepository;
+import com.example.springredditclone.repository.VoteRepository;
+import com.example.springredditclone.service.AuthService;
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
+//This Mapper is an ABSTRACT CLASS vs an INTERFACE because we need to add some extra dependencies for ADDITIONAL FIELDS FOR DTO
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+
+    //Required for comment count
+    @Autowired
+    private CommentRepository commentRepository;
+
+    //Required for Vote count
+    @Autowired
+    private VoteRepository voteRepository;
+
+    //Required for Calculating the votes For USER (if he has already voted)
+    @Autowired
+    private AuthService authService;
+
 
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "subreddit", source = "subreddit")
     @Mapping(target = "user", source = "user")
     @Mapping(target = "description", source = "postRequest.description")
-    Post mapDtoToPost(PostRequest postRequest, Subreddit subreddit, User user);
+    //New Added in DTO
+    @Mapping(target = "voteCount", constant = "0")
+    public abstract Post mapDtoToPost(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mapping(target = "id", source = "postId")
-    @Mapping(target = "postName", source = "postName")
-    @Mapping(target = "description", source = "description")
-    @Mapping(target = "url", source = "url")
     @Mapping(target = "subredditName", source = "subreddit.name")
     @Mapping(target = "userName", source = "user.userName")
-    PostResponse mapPostToDto(Post post);
+    //New Added in DTO
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapPostToDto(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 }
