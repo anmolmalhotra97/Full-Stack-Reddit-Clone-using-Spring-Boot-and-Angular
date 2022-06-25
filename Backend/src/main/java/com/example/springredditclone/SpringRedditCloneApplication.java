671513a067b732457bf6d4e918d89536d6481b7b
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-06-25 01:31:22 -0400

    Added Swagger & SpringFox for API Documention

--- Backend/src/main/java/com/example/springredditclone/SpringRedditCloneApplication.java
+++ Backend/src/main/java/com/example/springredditclone/SpringRedditCloneApplication.java
@@ -1,13 +1,16 @@
 package com.example.springredditclone;
 
+import com.example.springredditclone.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @EnableAutoConfiguration
+@Import(SwaggerConfiguration.class)
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringRedditCloneApplication {
 
